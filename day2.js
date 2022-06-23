d22156c8450b9484d7076d1305168ed20d10dfbc
PhamHoangAn92 <anphaomega3@gmail.com>
2022-06-23 15:51:45 +0700

    Update file

--- day2.js
+++ day2.js
@@ -140,28 +140,3 @@ function number_array(array, x){
 
 
 
-/*Tạo hàm đếm số lần xuất hiện của một phần tử trong mảng JavaScript*/
-// var array= [5, 2, 3, 5, 2, 3, 2, 4];
-// function number_array(array, x){
-//     let count = 0;
-//     for(let i = 0; i< array.length; i++){
-//       if(array[i]==x) //Tìm thấy phần tử giống x trong mảng thì cộng biến đếm
-//         count ++;
-//     }
-//     console.log( "Phan tu " +  x  + " xuat hien " + count +  " lan");
-// }
-
-
-// /*Xóa phần tử trùng nhau và lấy các phần tử duy nhất*/
-// let remove_Duplicates = array.reduce(function (accumulator, element) {
-//   if (accumulator.indexOf(element) === -1) {
-//     accumulator.push(element)
-//   }
-//   return accumulator;
-// }, [])
-// console.log(remove_Duplicates);
-
-
-// /*đếm số lần xuất hiện của các phần tử duy nhất*/
-// for (let i = 0; i < remove_Duplicates.length; i++) 
-//     number_array(array, remove_Duplicates[i]);
